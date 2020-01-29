f92b3b281f98b7a44ad2355acb2e74d4b6996ba6
justinbaker10 <justin-baker@live.com>
2020-01-28 21:10:11 -0600

    second commit - functions

--- exercises/102-undefined-booleans-null.js
+++ exercises/102-undefined-booleans-null.js
@@ -6,6 +6,10 @@
 // 2) Declare a variable "huh" and assign nothing to it, then return "huh".
 //    NOTE: the value undefined means "lack of assignment", which is what this
 //          function should return
+function makeNothing () {
+    var huh
+    return huh
+}
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -13,18 +17,31 @@
 // 2) Declare a variable "myBool" and assign it either true or false, then return "myBool"
 //    NOTE: remember that the string "true" (surrounded by quotes) is not
 //          the same thing as boolean true (no quotes)
-
+function makeBoolean () {
+    var myBool = true
+    return myBool
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // 1) Create a function "makeTrue"
 // 2) Declare a variable "yup" and assign boolean true, then return "yup"
-
+function makeTrue () {
+    var yup = true
+    return yup
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // 1) Create a function "makeFalse"
 // 2) Declare a variable "nope" and assign boolean false, then return "nope"
-
+function makeFalse () {
+    var nope = false
+    return nope
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // 1) Create a function "makeNull"
 // 2) Declare a variable "nothingMuch" and assign null to it, then return "nothingMuch"
+function makeNull () {
+    var nothingMuch = null
+    return nothingMuch
+}
